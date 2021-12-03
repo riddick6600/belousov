68c038368f6062db256674bff2c556d114131bb2
Дерябин Георгий Андреевич <gderyabin@mtsbank.ru>
2021-12-03 12:37:45 +0300

    init

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps }
+    }
+
+    render() {
+        const {locale} = this.props.__NEXT_DATA__ 
+        const dir = locale === 'ar' ? 'rtl' : 'ltr';
+        return (
+            <Html dir={dir} lang={locale}>
+                <Head>
+                    <link rel="icon" type="image/png" href="/images/favicon.png"></link>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument;
