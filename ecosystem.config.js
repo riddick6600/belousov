b1b569a91190b8676fe3b48f7ee72f604b1363eb
Дерябин Георгий Андреевич <gderyabin@mtsbank.ru>
2021-12-08 02:02:05 +0300

    ecos

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,26 @@
 module.exports = {
-  apps : [{
-    name: 'Next',
-    script: 'npm start',
-    instances: 1,
-    autorestart: true,
-    watch: true,
-    env: {
-      PORT: '80',
-      NODE_ENV: 'production'
-    }
-  }],
+  apps: [
+    {
+      name: "Next",
+      cwd: "/opt/belousov",
+      script: "npm start",
+      autorestart: true,
+      env: {
+        PORT: "80",
+        NODE_ENV: "production",
+      },
+    },
+  ],
 
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+  deploy: {
+    production: {
+      user: "node",
+      host: "212.83.163.1",
+      ref: "origin/master",
+      repo: "git@github.com:repo.git",
+      path: "/var/www/production",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production",
+    },
+  },
 };
