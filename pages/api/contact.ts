2739487f3b9dee4ea00828151d7acea4b2e046c8
Дерябин Георгий <gaderyabin@www.mts.ru>
2022-07-22 00:51:59 +0300

    update :

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -2,14 +2,8 @@ import type { NextApiRequest, NextApiResponse } from "next";
 const fs = require("fs");
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  fs.appendFile(
-    "/var/log/phones.log",
-    `Телефон ${req.body.number}\n`,
-    () => {
-      res.status(200).json({ status: "ok" });
-    },
-    () => {
-      res.status(500).json({ status: "Fail to save file" });
-    }
-  );
+  fs.appendFile("/var/log/phones.log", `Телефон ${req.body.number}\n`, () => {
+    res.status(500).json({ status: "Fail to save file" });
+  });
+  res.status(200).json({ status: "ok" });
 };
