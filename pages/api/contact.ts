8cd69fb5d465bd333916dde26633f8fc3d3ce549
Георгий Дерябин <gderyabin@mtsbank.ru>
2022-07-27 14:53:03 +0300

    config

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,16 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 const fs = require("fs");
 
-const templateUrl = new URL(
-  "https://za-it.bitrix24.ru/rest/1/ae0hygmqx539sfic/crm.lead.add.json"
-);
-templateUrl.searchParams.append("FIELDS[TITLE]", "Заявка с сайта");
+const templateUrl =
+  "https://za-it.bitrix24.ru/rest/1/ae0hygmqx539sfic/crm.lead.add.json";
 
 //?FIELDS[TITLE]=Заявка с сайта&FIELDS[PHONE][0][VALUE]=
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const fetchUrl = templateUrl;
+  const fetchUrl = new URL(templateUrl);
+  fetchUrl.searchParams.append("FIELDS[TITLE]", "Заявка с сайта");
   fetchUrl.searchParams.append("FIELDS[PHONE][0][VALUE]", req.body.number);
-  const response = await fetch(fetchUrl);
+  const response = await fetch(fetchUrl.toString());
   res.status(200).json({ status: "ok" });
+  return null;
 };
